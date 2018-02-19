1c0167f12970a2e511596f65f227016c1566d7fa
udon <naoki_udon@outlook.com>
2018-02-19 19:41:43 +0900

    ノーツ構成をざっくりと

---
+++ Assets/Scripts/Games/SoundNote.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UdonLib.Commons;
+
+[]
+public class SoundNote : UdonBehaviour {
+
+    private SoundNoteImage _noteImage;
+    private float _timing;
+
+}
