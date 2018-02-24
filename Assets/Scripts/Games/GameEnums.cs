13ea0c5dea5def481c35f388437701e6ca1a32d6
udon <naoki_udon@outlook.com>
2018-02-24 20:53:45 +0900

    ノーツの構成のざっくりとした仕様を殴り書き

---
+++ Assets/Scripts/Games/GameEnums.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// ゲーム中で使う列挙型
+/// </summary>
+namespace CrossRhythm.GameEnums{
+
+    /// <summary>
+    /// ノーツのレーン位置
+    /// </summary>
+    public enum NotePlace
+    {
+        NONE,//不定
+        UPPER_LEFT,//左上
+        UPPER_RIGHT,//右上
+        LOWER_LEFT,//左下
+        LOWER_RIGHT,//右下
+    }
+
+}
--- Assets/Scripts/Games/SoundNote.cs
+++ Assets/Scripts/Games/SoundNote.cs
@@ -1,13 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using CrossRhythm.GameEnums;
 using UdonLib.Commons;
 
-[]
 public class SoundNote : UdonBehaviour {
 
-    private SoundNoteImage _noteImage;
-    private float _timing;
+    private NotePlace _place;
+    public NotePlace Place => _place;
 
+    private float _timing;
+    public float Timing => _timing;
+
+    private SoundNoteImage _noteImage;
+
+    public SoundNote(SoundNoteImage noteImage, float timing)
+    {
+        _noteImage = noteImage;
+        _timing = timing;
+        SetOnDestroy(OnDestroy);
+    }
+
+    public void SetPosition()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+
+    }
 }
