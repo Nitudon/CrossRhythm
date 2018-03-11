2bc60a925005223d84a2eae97bb7184a36069c75
udon <naoki_udon@outlook.com>
2018-03-11 16:04:14 +0900

    ノーツプレハブの概形と移動機構の概形を作成

--- Assets/Scripts/Games/SoundNoteMover.cs
+++ Assets/Scripts/Games/SoundNoteMover.cs
@@ -1,8 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UdonLib.Commons;
+using UniRx;
 
-public class SoundNoteMover : MonoBehaviour {
+[RequireComponent(typeof(SoundNote))]
+public class SoundNoteMover : UdonBehaviour,IDisposable {
 
-	
+    [SerializeField]
+    private SoundNote _note;
+
+    private IDisposable _disposable;
+
+	public void Initialize()
+    {
+
+    }
+
+    public void Dispose()
+    {
+
+    }
+
+    private void NoteMove()
+    {
+
+    }
+
 }
