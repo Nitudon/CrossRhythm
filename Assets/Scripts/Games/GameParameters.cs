86871104b761580e8a4db7e2ff704c74138cbd99
udon <naoki_udon@outlook.com>
2018-03-14 00:14:44 +0900

    ノーツの開始位置を定義

--- Assets/Scripts/Games/GameParameters.cs
+++ Assets/Scripts/Games/GameParameters.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
     using GameEnums;
+    using UnityEngine;
 
     public static class GameParameters
     {
@@ -9,12 +10,20 @@
 
         public readonly static int MAX_SCORE = 1000000;
 
-        public static Dictionary<JudgeGrade, float> JUDGE_COEFFICIENT = new Dictionary<JudgeGrade, float>
+        public readonly static Dictionary<JudgeGrade, float> JUDGE_COEFFICIENT = new Dictionary<JudgeGrade, float>
         {
             {JudgeGrade.PERFECT, 1.0f },
             {JudgeGrade.GREAT, 0.7f },
             {JudgeGrade.GOOD, 0.35f },
             {JudgeGrade.MISS, 0f },
+        };
+
+        public readonly static Dictionary<NotePlace, Vector3> NOTE_PLACES = new Dictionary<NotePlace, Vector3>
+        {
+            {NotePlace.LOWER_LEFT, new Vector3(-880f, -545f, 0f) },
+            {NotePlace.LOWER_RIGHT, new Vector3(880f, -545f, 0f) },
+            {NotePlace.UPPER_LEFT, new Vector3(-880f, 545f, 0f) },
+            {NotePlace.UPPER_RIGHT, new Vector3(880f, 545f, 0f) },
         };
     }
 }
