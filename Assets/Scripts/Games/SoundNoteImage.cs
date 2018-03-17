3beaa688a3bcadd20e3cdf5507fe429422e6df2e
udon <naoki_udon@outlook.com>
2018-03-17 17:37:53 +0900

    譜面データとノーツデータの保存場所と読み出しの作成

--- Assets/Scripts/Games/SoundNoteImage.cs
+++ Assets/Scripts/Games/SoundNoteImage.cs
@@ -1,7 +1,22 @@
 ﻿using UnityEngine;
+using CrossRhythm.GameEnums;
 
+[CreateAssetMenu(menuName = "CrossRhythm/SoundNoteData")]
 public class SoundNoteImage : ScriptableObject{
 
+    private const string PATH_PREFIX = "NoteDatas/";
+
     private Texture2D _texture;
+
+    public static SoundNoteImage Load(NoteType type)
+    {
+        var note = Resources.Load<SoundNoteImage>($"{PATH_PREFIX}note_{type}");
+        if (note == null)
+        {
+            Debug.Log("ノーツ情報を読み込めませんでした");
+        }
+
+        return null;
+    }
 
 }
