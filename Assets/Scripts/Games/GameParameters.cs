18cf38838be39f28f10c8df36572b3506755fcb1
udon <naoki_udon@outlook.com>
2018-03-18 23:07:13 +0900

    場所を配列に変更し、移動速度係数と生成オフセット係数を作成、Music拡張

--- Assets/Scripts/Games/GameParameters.cs
+++ Assets/Scripts/Games/GameParameters.cs
@@ -10,6 +10,10 @@
 
         public readonly static int MAX_SCORE = 1000000;
 
+        public readonly static float DEFAULT_MOVE_INTERVAL = 2.0f;
+
+        public readonly static float DEFAULT_PRE_CREATE_INTERVAL = 2.5f;
+
         public readonly static Dictionary<JudgeGrade, float> JUDGE_COEFFICIENT = new Dictionary<JudgeGrade, float>
         {
             {JudgeGrade.PERFECT, 1.0f },
@@ -18,12 +22,12 @@
             {JudgeGrade.MISS, 0f },
         };
 
-        public readonly static Dictionary<NotePlace, Vector3> NOTE_PLACES = new Dictionary<NotePlace, Vector3>
+        public readonly static Vector3[] NOTE_PLACES = 
         {
-            {NotePlace.LOWER_LEFT, new Vector3(-880f, -545f, 0f) },
-            {NotePlace.LOWER_RIGHT, new Vector3(880f, -545f, 0f) },
-            {NotePlace.UPPER_LEFT, new Vector3(-880f, 545f, 0f) },
-            {NotePlace.UPPER_RIGHT, new Vector3(880f, 545f, 0f) },
+            new Vector3(-880f, -545f, 0f),
+            new Vector3(880f, -545f, 0f),
+            new Vector3(-880f, 545f, 0f),
+            new Vector3(880f, 545f, 0f),
         };
     }
 }
